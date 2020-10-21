3e3c54f93e4335d89d42bd1ff26f71d992753ab9
aditya-shirwatkar <cosmosaditya02@gmail.com>
2020-10-21 23:01:55 +0530

    added solve

--- controllers/direct_trajopt_contact.py
+++ controllers/direct_trajopt_contact.py
@@ -4,6 +4,7 @@
 import casadi as ca
 import numpy as np
 from contact_trajectory_optimization.envs.finger_contact import FingerContact
+from matplotlib import pyplot as plt
 
 
 class NLP:
@@ -27,25 +28,29 @@ class NLP:
         self.h = self.opti.variable(1)
         self.opti.subject_to(self.h > 0)
         self.opti.subject_to(self.h <= self.model.dt)
-
+        self.opti.set_initial(self.h, 0.05)
         self.states = []
         self.dstates = []
         self.actions = []
         self.forces, self.gammas = [], []
 
-        for i in range(self.N + 1):
+        for i in range(self.N):
             self.states.append(self.opti.variable(self.model.n_joints, 1))
             self.dstates.append(self.opti.variable(self.model.n_joints, 1))
             # if i == 0:
             #     self.actions.append(ca.DM.zeros(self.model.dof, 1))
             #     self.forces.append(ca.DM.zeros(self.model.dims, self.model.n_contact))
             # else:
-            if i > 0:
-                self.actions.append(self.opti.variable(self.model.dof, 1))
-                self.forces.append(self.opti.variable(self.model.dims + 1, self.model.n_contact))
-                self.gammas.append(self.opti.variable(1))
+            # if i > 0:
+            self.actions.append(self.opti.variable(self.model.dof, 1))
+            self.forces.append(self.opti.variable(self.model.dims + 1, self.model.n_contact))
+            self.gammas.append(self.opti.variable(1))
 
     def __setConstraints__(self):
+        # self.opti.subject_to(self.states[0] == [np.pi / 3, -np.pi / 3, 0])
+        self.opti.subject_to(self.dstates[0] == [0]*3)
+        self.opti.subject_to(self.dstates[-1][-1] == 0.5)
+
         for k in range(self.N - 1):
             q_1, dq_1 = self.states[k], self.dstates[k]
             q_2, dq_2 = self.states[k + 1], self.dstates[k + 1]
@@ -63,8 +68,8 @@ class NLP:
             f_2 = self.model.dynamics(q=q_2, dq=dq_2, lam=lam_2)
 
             self.opti.subject_to(q_1 - q_2 + self.h * dq_2 == 0)
-            self.opti.subject_to(f_2['H'] @ (dq_2 - dq_1) + self.h * f_2['C'] @ (q_2 - q_1) - f_2['B'] @ u_2 - f_2[
-                'J_phi'].T @ lam_2 == 0)
+            self.opti.subject_to(f_2['H'] @ (dq_2 - dq_1) +
+                                 f_2['C'] @ (q_2 - q_1) + self.h * (- f_2['B'] @ u_2 - f_2['J_phi'].T @ lam_2) == 0)
 
             # friction constraints
             lam_1_z, lam_1_xp, lam_1_xm = self.forces[k][2, 0], self.forces[k][0, 0], self.forces[k][1, 0]
@@ -75,23 +80,31 @@ class NLP:
             self.opti.subject_to(self.model.mu * lam_1_z - lam_1_xp - lam_1_xm >= 0)
             self.opti.subject_to(gam_1 + psi_1 >= 0)
             self.opti.subject_to(gam_1 - psi_1 >= 0)
-            self.opti.subject_to(f_1['phi'].T @ lam_1_z == 0)
+
+            self.opti.subject_to(f_1['phi'].T @ lam_1_z >= 0)
+            self.opti.subject_to(f_1['phi'].T @ lam_1_z <= 0)
+
             self.opti.subject_to((self.model.mu * lam_1_z - lam_1_xp - lam_1_xm).T @ lam_1_z == 0)
-            # self.opti.subject_to((gam_1 + psi_1).T @ lam_1_xp == 0)
-            # self.opti.subject_to((gam_1 - psi_1).T @ lam_1_xm == 0)
+
+            self.opti.subject_to((gam_1 + psi_1).T @ lam_1_xp <= 0)
+            self.opti.subject_to((gam_1 + psi_1).T @ lam_1_xp >= 0)
+
+            self.opti.subject_to((gam_1 - psi_1).T @ lam_1_xm <= 0)
+            self.opti.subject_to((gam_1 - psi_1).T @ lam_1_xm >= 0)
 
             # bounds model specific
-            # self.opti.subject_to(ca.fabs(q_1) <= np.pi)
-            # self.opti.subject_to(ca.fabs(q_2) <= np.pi)
+            # self.opti.bounded([-np.pi]*3, q_1, [np.pi]*3)
+            self.opti.subject_to(q_1 <= np.pi)
+            self.opti.subject_to(q_1 >= -np.pi)
 
     def __setCosts__(self):
-        Q = ca.diag(ca.MX([0, 0, 1]))
+        Q = ca.diag(ca.MX([0, 0, 0]))
         R = ca.diag(ca.MX([1, 1]))
         cost = 0
-        for k in range(1, self.N):
+        for k in range(self.N):
             q, dq = self.states[k], self.dstates[k]
             u = self.actions[k]
-            cost += q.T @ Q @ q + u.T @ R @ u
+            cost += dq.T @ Q @ dq + u.T @ R @ u
 
         self.opti.minimize(cost)
 
@@ -99,8 +112,32 @@ class NLP:
         p_opts = {"expand": True}
         s_opts = {"max_iter": 3000}
         self.opti.solver("ipopt", p_opts, s_opts)
-        self.solution = self.opti.solve()
+        self.solution = self.opti.solve_limited()
+
+    def __interpolate__(self):
+        k = 0.005
+        self.x1 = []
+        self.x2 = []
+        self.x3 = []
+        self.dt = self.solution.value(self.h)
+        self.u1 = []
+        self.u2 = []
+        for i in range(self.N):
+            self.x1.append(self.solution.value(self.states[i][0]))
+            self.x2.append(self.solution.value(self.states[i][1]))
+            self.x3.append(self.solution.value(self.states[i][2]))
+
+        self.__plot__()
+
+    def __plot__(self):
+        t = np.linspace(0, self.N * self.dt, self.N)
+        plt.plot(t, self.x1, 'o')
+        plt.plot(t, self.x2, 'o')
+        plt.plot(t, self.x3, 'o')
+
+        plt.show()
 
 
 Problem = NLP()
 Problem.__solve__()
+Problem.__interpolate__()
